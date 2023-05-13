7b0113aaf83effa9e51f0b6d0a2b5416a9616cd4
amine <bougassaa@gmail.com>
2023-05-13 22:03:50 +0200

    change title

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include_once "layouts/header.php"
 
 <div class="p-3 align-items-end align-items-md-center justify-content-center" id="landing-home">
     <div class="text-light">
-        <h1>Nuances Façade, Ravalement de façade autour de Valréas</h1>
+        <h1>Nuances Façade, façadier spécialisé</h1>
         <p class="text-justify">
             Embellissez votre maison avec notre entreprise de <a class="link-light" href="/ravalement-facade">ravalement de façade</a> de qualité ! Nous sommes fiers de desservir les régions Drome, Vaucluse, Gard et Ardèche, et nous offrons une expertise dans la rénovation et le neuf, ainsi que dans le nettoyage de façade ancienne.
         </p>
