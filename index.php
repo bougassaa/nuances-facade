c4c4af43d9dd4f5f579ca8d3f4e3341e78f0c66f
amine <a.bougassaa@abo-s.com>
2023-07-12 15:36:35 +0200

    finish google reviews

--- index.php
+++ index.php
@@ -99,10 +99,85 @@ include_once "layouts/header.php"
             Nous sommes également disponibles pour répondre à toutes vos questions et pour vous fournir des conseils utiles sur l'entretien de votre façade à long terme. N'hésitez pas à <a href="/contact">nous contacter</a> pour tous vos besoins en matière de ravalement de façade et de nettoyage de toiture.
         </p>
     </div>
-    <h2 class="mb-4 mt-5">Les retours positifs de nos clients</h2>
+    <hr class="my-5" />
+    <div class="row py-2">
+        <div class="col-12 text-center">
+            <img class="google-review" src="/images/google-reviews/google-review-logo.png" alt="google reviews">
+        </div>
+    </div>
+    <div class="row my-3">
+        <div class="col-md-6 col-lg-3 mb-md-0 mb-3">
+            <div class="card h-100">
+                <div class="card-body">
+                    <div class="d-flex align-items-center gap-2 position-relative">
+                        <img class="avatar-review" src="/images/google-reviews/avatar-clotilde.png" alt="avatar clo">
+                        <div class="d-flex flex-column">
+                            <span class="fw-semibold">Clotilde</span>
+                            <img class="stars-review" src="/images/google-reviews/stars.png" alt="stars reviews">
+                        </div>
+                        <small class="text-muted position-absolute end-0">21/06/2023</small>
+                    </div>
+                    <div class="mt-3 text-secondary">
+                        Facadier très professionnel. Travail sérieux et personnel agréable. Bon rapport qualité / prix.<br>Je recommande cette entreprise.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-lg-3 mb-md-0 mb-3">
+            <div class="card h-100">
+                <div class="card-body">
+                    <div class="d-flex align-items-center gap-2 position-relative">
+                        <img class="avatar-review" src="/images/google-reviews/avatar-cyril.png" alt="avatar clo">
+                        <div class="d-flex flex-column">
+                            <span class="fw-semibold">Cyril A.</span>
+                            <img class="stars-review" src="/images/google-reviews/stars.png" alt="stars reviews">
+                        </div>
+                        <small class="text-muted position-absolute end-0">27/05/2023</small>
+                    </div>
+                    <div class="mt-3 text-secondary">
+                        Entreprise sérieuse, très agréable, travail de qualité, belle finition. Chantier très propre.<br>Je recommande sincèrement l’entreprise Nuances façade
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-lg-3 mb-md-0 mb-3">
+            <div class="card h-100">
+                <div class="card-body">
+                    <div class="d-flex align-items-center gap-2 position-relative">
+                        <img class="avatar-review" src="/images/google-reviews/avatar-thimote.png" alt="avatar clo">
+                        <div class="d-flex flex-column">
+                            <span class="fw-semibold">Thimote H.</span>
+                            <img class="stars-review" src="/images/google-reviews/stars.png" alt="stars reviews">
+                        </div>
+                        <small class="text-muted position-absolute end-0">15/05/2023</small>
+                    </div>
+                    <div class="mt-3 text-secondary">
+                        Nous avons fait appel à ce façadier pour nos clôtures et abris de jardin. Nous sommes très ravie du résultat. Professionnel très arrangeant, travail propre
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-lg-3 mb-md-0 mb-3">
+            <div class="card h-100">
+                <div class="card-body">
+                    <div class="d-flex align-items-center gap-2 position-relative">
+                        <img class="avatar-review" src="/images/google-reviews/avatar-manuelle.png" alt="avatar clo">
+                        <div class="d-flex flex-column">
+                            <span class="fw-semibold">Manuelle P.</span>
+                            <img class="stars-review" src="/images/google-reviews/stars.png" alt="stars reviews">
+                        </div>
+                        <small class="text-muted position-absolute end-0">26/06/2023</small>
+                    </div>
+                    <div class="mt-3 text-secondary">
+                        Équipe très soigneuse et sérieuse. Résultats au top ! Je suis ravie d avoir fais affaire avec Nuances Façade.<br>Délai, ponctualité, professionnalisme, tarifs.  Merci
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="row">
         <div class="col-12 text-center">
-            <img class="" src="/images/logo/google-review.png" alt="google reviews">
+            <a href="https://g.page/r/CapAaNx-0yS-EB0/review">Vous pouvez nous laisser un avis ici</a> ⭐
         </div>
     </div>
 </div>
